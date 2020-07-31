1dce31c49d7f1f9414bf14d11a06f343e4dd2d6f
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2020-07-31 23:22:51 +0900

    マージ作ってNが2の累乗のときはできるようになったワヨ

--- merge.js
+++ merge.js
@@ -1,4 +1,5 @@
-const N = 8;
+const N = 16;
+const TOO_BIGGER_NUM = N * 10000;
 
 const genereteData = () => {
     let numbers = [...Array(N)].map((val, index) => index + 1);
@@ -13,7 +14,7 @@ const genereteData = () => {
 
 const divide = (data) => {
     if (data.length <= 2) {
-        return data;
+        return [...data];
     }
     const half = Math.floor(data.length / 2);
     const left = data.slice(0, half);
@@ -21,6 +22,40 @@ const divide = (data) => {
     return [[...divide(left)], [...divide(right)]];
 };
 
+const merge = (left, right) => {
+    if (typeof left[0] === 'number' && typeof right[0] === 'number') {
+        if (left.length <= 2) {
+            const reverse = (numbers) => {
+                if (numbers.length === 2 && numbers[0] > numbers[1]) {
+                    return [numbers[1], numbers[0]];
+                }
+                return [...numbers];
+            };
+            left = reverse(left);
+            right = reverse(right);
+        }
+        let leftIndex = 0, rightIndex = 0;
+        left = [...left, TOO_BIGGER_NUM];
+        right = [...right, TOO_BIGGER_NUM];
+        let result = [];
+        while (rightIndex < right.length - 1 || leftIndex < left.length - 1) {
+            const numL = left[leftIndex], numR = right[rightIndex];
+            if (numL < numR) {
+                result = [...result, numL];
+                leftIndex++;
+            } else {
+                result = [...result, numR];
+                rightIndex++;
+            }
+        }
+        return result;
+    } else {
+        return merge(merge(left[0], left[1]), merge(right[0], right[1]));
+    }
+};
+
 const data = genereteData();
 const division = divide(data);
-console.log(division);
+const result = merge(division[0], division[1]);
+console.log(data);
+console.log(result);
