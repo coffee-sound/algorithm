dd7f127894e6a6ad2e4ea79825d2cb450beeef83
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2020-08-02 05:39:28 +0900

    はい勝ち〜

--- merge-battle/bo.py
+++ merge-battle/bo.py
@@ -5,12 +5,5 @@ print(data)
 
 # 俺の戦いはここから始まるぜ
 divide = lambda data: data if len(data) == 1 else [min(data), max(data)] if len(data) <= 2 else [divide([data[i] for i in range(len(data) // 2)]), divide([data[i] for i in range(len(data) // 2, len(data))])]
-def merge(left, right):
-    if type(left[0]) is int and type(right[0]) is int:
-        indexes, left, right, result = [0, 0], left + [0xFFFF], right + [0xFFFF], []
-        while indexes[0] < len(left) - 1 or indexes[1] < len(right) - 1:
-            result.append(left[indexes[0]] if left[indexes[0]] < right[indexes[1]] else right[indexes[1]])
-            indexes[0 if left[indexes[0]] < right[indexes[1]] else 1] += 1
-        return result
-    return merge(left, merge(right[0], right[1])) if type(left[0]) is int else (merge(merge(left[0], left[1]), right) if type(right[0]) is int else merge(merge(left[0], left[1]), merge(right[0], right[1])))
+merge = lambda left, right: [right.pop(0) if len(left) == 0 else left.pop(0) if len(right) == 0 else left.pop(0) if left[0] < right[0] else right.pop(0) for i in range(len(left) + len(right))] if type(left[0]) is int and type(right[0]) is int else merge(left, merge(right[0], right[1])) if type(left[0]) is int else (merge(merge(left[0], left[1]), right) if type(right[0]) is int else merge(merge(left[0], left[1]), merge(right[0], right[1])))
 print(merge(divide(data)[0], divide(data)[1]))
