ffc5109b3da41b4ce9bd5f510bd68c68b9bcf4f7
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2020-08-01 14:34:43 +0900

    これから始まるぜ

--- merge-battle/bo.py
+++ merge-battle/bo.py
@@ -1,5 +1,6 @@
 from data import generate_data
 
 data = generate_data() # 1〜10の数値がバラバラに並んでる
+print(data)
 
 # 俺の戦いはここから始まるぜ
--- merge-battle/coffee.py
+++ merge-battle/coffee.py
@@ -1,5 +1,6 @@
 from data import generate_data
 
 data = generate_data() # 1〜10の数値がバラバラに並んでる
+print(data)
 
 # コーヒー王国の意地がここにある
